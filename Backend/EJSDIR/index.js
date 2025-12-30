5ec50990d8d0772c50e62e82b244bb3ba1e971b4
Avneesh Sharma <avneesh149m@gmail.com>
2025-12-30 10:26:00 +0530

    Added

--- Backend/EJSDIR/index.js
+++ Backend/EJSDIR/index.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
+const path = require('path')
 app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "/views"))
 
-app.get('/home',(req,res)=>{
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/', (req, res) => {
+    res.send('Helooo')
+})
+app.get('/home', (req, res) => {
     res.render('home')
 })
-app.get('/about',(req,res)=>{
+app.get('/diceroll', (req, res) => {
+    let diceval = Math.floor(Math.random() * 6) + 1
+    res.render('diceroll', { diceval })
+})
+app.get('/about', (req, res) => {
     res.render('about')
+})
+// app.get('/ig/:username',(req,res)=>{
+//     let {username} = req.params;
+//     let followers = ['kashish','Avneesh','Instagram','viral']
+//     res.render('instagram',{username,followers})
+// })
+app.get('/ig/:username', (req, res) => {
+    let { username } = req.params;
+    let instaData = require('./data.json');
+    let data = instaData[username];
+    // console.log(data)
+    if (data)
+        res.render('instagram', { data })
+    else
+        res.send(`<h1>No such Data for @${username}</h1>`)
 })
 
 app.listen(port, () => {
